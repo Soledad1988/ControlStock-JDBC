2a0da234c977d7d9d15ffc5f1e539b3000767cc1
Brenda <brentorres26@gmail.com>
2023-04-29 07:02:42 -0300

    Clase principal

---
+++ controlStock/src/main/java/ControlDeStockMein.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+import vista.ControlDeStockFrame;
+
+public class ControlDeStockMein {
+
+	public static void main(String[] args) {
+		
+			ControlDeStockFrame produtoCategoriaFrame = new ControlDeStockFrame();
+			produtoCategoriaFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+	}
+}
